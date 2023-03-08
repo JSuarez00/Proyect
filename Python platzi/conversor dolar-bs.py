6ad6e6286e8dc6fa34ebea0fee5a095733dad0c4
unknown <2016jasz@gmail.com>
2023-03-08 15:06:47 -0400

    proyecto primera v1

--- Python platzi/conversor dolar-bs.py
+++
@@ -1,6 +0,0 @@
-dolares = int (input("cuantos dolares tienes: "))
-bolivares = 25000
-dolar= dolares * bolivares
-dolar= round(dolar, 2)
-dolar= str(dolar)
-print("Tienes bs "+ dolar + " bolivares")
