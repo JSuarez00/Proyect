cfbb0f4ad16603733911e9c1b856c881e88237e3
Jasz <2016jasz@gmail.com>
2023-07-24 08:33:17 -0400

    problema en postgrest error django.db.utils.OperationalError

--- django/tiendapratica/gestionpedidos/models.py
+++ django/tiendapratica/gestionpedidos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class proyecto(models.Model):
+    nombre=models.CharField(max_length=30)
+    descripcion=models.CharField(max_length=200)
+    imagen=models.ImageField()
+    link=models.URLField(None)
+    web=models.BooleanField(None)
+    ciencia_de_datos=models.BooleanField(None)
+    analisis_de_datos=models.BooleanField(None)
+    tranding=models.BooleanField(None)
+    
--- django/tiendapratica/tiendapratica/settings.py
+++ django/tiendapratica/tiendapratica/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gestionpedidos'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'tiendapratica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portadb',
+        'USER':'postgres',
+        'PASSWORD':'Jasz.1997',
+        'HOST':'127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
